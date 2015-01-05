eee4343ec1e48cf22bc03ea75c18e417aaa177a0
206449354 <VictorLantigua@NUFHIANUDM016ML.local>
2015-01-04 21:58:50 -0500

    Updated login page

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,43 +1,94 @@
 <!DOCTYPE html>
-<html class="">
+<html>
     <head>
         <meta charset="UTF-8">
-        <title><?php echo APP_NAME; ?> Bug Tracker</title>
+        <title><?php echo APP_NAME; ?> Bug Tracker | Log In</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <!-- Theme style -->
-        <link href="/assets/css/AdminLTE.css" rel="stylesheet" type="text/css" />
-
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
+        <style>
+            html, body {
+                background: #f1f1f1;
+            }
+
+            #login-box {
+              width: 400px;
+              margin: 90px auto 0 auto;
+            }
+
+            #login-box form {
+                margin-top: 20px;
+                margin-left: 0;
+                margin-bottom: 100px;
+                padding: 26px 24px 46px;
+                font-weight: 400;
+                overflow: hidden;
+                background: #fff;
+                -webkit-box-shadow: 0 1px 3px rgba(0,0,0,.13);
+                box-shadow: 0 1px 3px rgba(0,0,0,.13);
+            }
+
+            #login-box .header {
+                text-align: center;
+                font-size: 60px;
+                font-weight: bold;
+                color: #444;
+            }
+
+            #login-box .form-group i {
+                color: #CCCCCC;
+                display: block !important;
+                font-size: 20px;
+                height: 16px;
+                margin: 16px 2px 4px 10px;
+                position: absolute !important;
+                text-align: center;
+                width: 16px;
+                z-index: 1;
+            }
+
+            #login-box .form-group input {
+                font-size: 20px;
+                padding: 25px 25px 25px 40px;
+            }
+
+            #login-box button {
+                font-size: 20px;
+                background: #3c8dbc;
+                color: #fff;
+            }
+
+            #login-box .alert {
+                font-size: 16px;
+            }
+        </style>
     </head>
-    <body class="">
+    <body>
+        <div id="login-box">
+            <div class="header"><?php echo APP_NAME; ?></div>
+            <?php include Kohana::find_file('views', 'shared/flash_message'); ?>
 
-        <div class="form-box" id="login-box">
-            <div class="header bg-light-blue"><?php echo APP_NAME; ?></div>
             <form action="/users/login" method="post">
-                <div class="body bg-gray">
-                    <?php include Kohana::find_file('views', 'shared/flash_message'); ?>
-
+                <div class="body">
                     <div class="form-group">
+                        <i class="fa fa-user"></i>
                         <input type="text" name="username" class="form-control" placeholder="Email or SSO" required>
                     </div>
                     <div class="form-group">
+                        <i class="fa fa-lock"></i>
                         <input type="password" name="password" class="form-control" placeholder="Password" required>
                     </div>          
                 </div>
                 <div class="footer">                                                               
-                    <button type="submit" class="btn bg-light-blue btn-block">Sign in</button>                  
+                    <button type="submit" class="btn btn-block">Log In</button>                  
                 </div>
             </form>
         </div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
     </body>
 </html>
