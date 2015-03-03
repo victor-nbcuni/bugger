3aad7817789823423e4dadb95ee75f304235810d
206449354 <VictorLantigua@NUFHIANUDM016ML.local>
2015-03-03 15:01:32 -0500

    Updated issue page

--- application/views/issues/_view_attachments.php
+++ application/views/issues/_view_attachments.php
@@ -6,11 +6,11 @@
         <div class="dz-message text-muted" data-dz-message><h4>Click or drop files to upload.</h4></div>
         <?php foreach($issue->files->find_all() as $file): ?>
             <div class="col-xs-2">
-                <div class="alert">
+                <div style="text-align:center;">
                     <a class="fancybox" rel="gallery1" href="<?php echo $file->url(); ?>" title="<?php echo $file->filename; ?>">
                         <img class="img-thumbnail" src="<?php echo $file->url(); ?>">
                     </a>
-                    <a class="btn-remove-attachment" href="/issue_files/remove/<?php echo $issue->id; ?>?file_id=<?php echo $file->id; ?>"><i class="fa fa-trash"></i> Remove</a>
+                    <a class="btn-remove-attachment btn btn-sm btn-default" href="/issue_files/remove/<?php echo $issue->id; ?>?file_id=<?php echo $file->id; ?>"><i class="fa fa-trash"></i> Remove</a>
                 </div>
             </div>
         <?php endforeach; ?>
